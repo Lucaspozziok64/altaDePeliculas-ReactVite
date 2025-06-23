7c05a42dda938c2f0d737b027fd72d030b99e743
Lucasfi <lukafigueroa64@gmail.com>
2025-06-23 11:16:32 -0300

    agregamos boton agregar al form y estilo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
+import Formulario from './components/Formulario'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
 
   return (
     <>
-      <h1>Hola mundo</h1>
+      <main className='container my-2'>
+        <h1 className='text-white'>Peliculas</h1>
+        <section className='p-1 contenedorForm'>
+          <Formulario></Formulario>
+        </section>
+      </main>
     </>
   )
 }
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,26 @@
+import { Button } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+
+const Formulario = () => {
+  return (
+    <Form className="text-white formulario">
+      <Form.Group className="mb-3" controlId="form.ControlInput1">
+        <Form.Label className="mb-0">Nombre</Form.Label>
+        <Form.Control type="text" placeholder="ej Star Wars" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="form.ControlTextarea1">
+        <Form.Label className="mb-0">Descripcion</Form.Label>
+        <Form.Control as="textarea" rows={3} />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="form.ControlInput1">
+        <Form.Label className="mb-0">Genero</Form.Label>
+        <Form.Control type="text" placeholder="ej Ficcion" />
+      </Form.Group>
+      <div className="d-flex justify-content-end mx-2">
+        <Button variant="success" className=" rounded-3">Agregar</Button>
+      </div>
+    </Form>
+  );
+};
+
+export default Formulario;
