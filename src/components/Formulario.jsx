e64d63046b05d09a963a889c3f32624a989e5e02
Lucasfi <lukafigueroa64@gmail.com>
2025-06-23 12:02:32 -0300

    logramos agregar eventos al input del form e ir capturando los datos ingresados por usuario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,23 +1,33 @@
+import { useState } from "react";
 import { Button } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 
 const Formulario = () => {
+  const [nombre, setnombre] = useState('');
+  const [descripcion, setDescripcion] = useState('');
+  const [genero, setGenero] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('Aqui debo capturar los dartos del form')
+  }
+
   return (
-    <Form className="text-white formulario">
-      <Form.Group className="mb-3" controlId="form.ControlInput1">
+    <Form onSubmit={handleSubmit} className="text-white formulario">
+      <Form.Group className="mb-3" controlId="formBasicNombre">
         <Form.Label className="mb-0">Nombre</Form.Label>
-        <Form.Control type="text" placeholder="ej Star Wars" />
+        <Form.Control type="text" placeholder="ej Star Wars" onChange={(e)=> setnombre(e.target.value)} value={nombre} />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="form.ControlTextarea1">
+      <Form.Group className="mb-3" controlId="formBasicDescripcion">
         <Form.Label className="mb-0">Descripcion</Form.Label>
-        <Form.Control as="textarea" rows={3} />
+        <Form.Control as="textarea" rows={3} onChange={(e)=> setDescripcion(e.target.value)} value={descripcion} />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="form.ControlInput1">
+      <Form.Group className="mb-3" controlId="formBasicGenero">
         <Form.Label className="mb-0">Genero</Form.Label>
-        <Form.Control type="text" placeholder="ej Ficcion" />
+        <Form.Control type="text" placeholder="ej Ficcion" onChange={(e)=> setGenero(e.target.value)} value={genero}/>
       </Form.Group>
       <div className="d-flex justify-content-end mx-2">
-        <Button variant="success" className="rounded-3">Agregar</Button>
+        <Button variant="success" type="submit" className="rounded-3">Agregar</Button>
       </div>
     </Form>
   );
